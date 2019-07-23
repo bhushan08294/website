f100fb693288c715a6685de5177caf8d4128d658
Bhushan <10517074@mydbs.ie>
2019-07-23 13:44:44 +0100

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -13,4 +13,19 @@ let header=()=>{
   h.innerHTML=txt;
 
 }
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.'''''''''
+}
+  function signOut() {
+      alert('User signing out.');
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      alert('User signed out.');//check this succeeded
+    });
+  }
+
 header();

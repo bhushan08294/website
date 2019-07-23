462ffafb3703b12df94774ecd6e71a24cc7551f3
Bhushan <10517074@mydbs.ie>
2019-07-23 13:46:06 +0100

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -14,6 +14,7 @@ let header=()=>{
 
 }
 function onSignIn(googleUser) {
+	alert('signing in');
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());

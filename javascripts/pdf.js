5df937859bdff77f27d25048743198d5ebde8778
Bhushan <10517074@mydbs.ie>
2019-08-04 13:06:29 +0100

    pdf test

--- javascripts/pdf.js
+++ javascripts/pdf.js
@@ -1,22 +1,25 @@
-import PSPDFKit from "pspdfkit";
-
-// Add an approve button to the toolbar
-const approveButton = {
-  type: "custom",
-  title: "Approve Document",
-  onPress() { placeApprovedStamp(); }
-};
-
-const instance = await PSPDFKit.load({
+/**
+ * PSPDFKit.load() accepts a configuration object with the following required values:
+ *
+ * container {string | HTMLElement} — Selector or element where PSPDFKit will be mounted.
+ * documentId {string}  — Document ID to load.
+ * authPayload {Object} — JSON Web Token in the format `{ jwt: 'xxx.xxx.xxx' }`.
+ * instant {boolean}    — The trial release gives you the option to try PSPDFKit Instant —
+ *                        our real-time, cross-platform, sync engine — by setting `instant: true`.
+ */
+PSPDFKit.load({
   container: "#pspdfkit",
-  pdf: "/contract.pdf",
-  toolbarItems: [...PSPDFKit.defaultToolbarItems, approveButton]
-});
+  documentId: documentId,
+  authPayload: authPayload,
+  instant: true
+})
+  .then(function(instance) {
+    instance.totalPageCount; // => 10
 
-function placeApprovedStamp() {
-  instance.createAnnotation(new PSPDFKit.Annotations.StampAnnotation({
-    pageIndex: 0,
-    stampType: "Approved",
-    boundingBox: new PSPDFKit.Geometry.Rect({width: 150, height: 40})
-  }));
-}
+    const viewState = instance.viewState;
+    viewState.currentPageIndex; // => 0
+    instance.setViewState(viewState.set("currentPageIndex", 1));
+  })
+  .catch(function(error) {
+    console.error(error.message);
+  });
